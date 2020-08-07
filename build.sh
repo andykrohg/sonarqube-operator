e60d4cc6b85328ab54ba379b5d01221503f29ba9
James Harmison <jharmison@redhat.com>
2020-08-07 17:25:01 -0400

    Linting, molecule, and travis fixes

--- build.sh
+++ build.sh
@@ -1,29 +1,3 @@
-#!/bin/bash
-SDK_RELEASE=v0.17
-MINOR_RELEASE=0
-
-# Update latest Roles
-rm -rf roles
-mkdir roles
-git clone https://github.com/redhat-gpte-devopsautomation/ansible-operator-roles
-cp -R ansible-operator-roles/roles/postgresql-ocp ./roles
-cp -R ansible-operator-roles/roles/sonarqube-ocp ./roles
-cp ansible-operator-roles/playbooks/sonarqube.yaml ./playbook.yml
-rm -rf ansible-operator-roles
-
-# Now build the Operator
-operator-sdk build quay.io/gpte-devops-automation/sonarqube-operator:${SDK_RELEASE}.${MINOR_RELEASE}
-docker push quay.io/gpte-devops-automation/sonarqube-operator:${SDK_RELEASE}.${MINOR_RELEASE}
-
-operator-sdk build quay.io/gpte-devops-automation/sonarqube-operator:${SDK_RELEASE}.${MINOR_RELEASE}
-docker tag quay.io/gpte-devops-automation/sonarqube-operator:${SDK_RELEASE}.${MINOR_RELEASE} quay.io/gpte-devops-automation/sonarqube-operator:latest
-docker tag quay.io/gpte-devops-automation/sonarqube-operator:${SDK_RELEASE}.${MINOR_RELEASE} quay.io/gpte-devops-automation/sonarqube-operator:${SDK_RELEASE}
-docker push quay.io/gpte-devops-automation/sonarqube-operator:${SDK_RELEASE}.${MINOR_RELEASE}
-docker push quay.io/gpte-devops-automation/sonarqube-operator:${SDK_RELEASE}
-docker push quay.io/gpte-devops-automation/sonarqube-operator:latest
-
-
-
 #! /usr/bin/env bash
 
 # change this
@@ -118,9 +92,9 @@ function docker_build () {
     fi
 
     if [ -n "$OPERATOR_BUILD" ]; then
-        pip3 install --user git+https://git.jharmison.com/jharmison/operator-sdk-manager
+        pip3 install git+https://git.jharmison.com/jharmison/operator-sdk-manager
         operator-sdk-manager update -vv
-        operator-sdk build --image-build-args "${args[*]}" "$CONTAINER_IMAGE:$CONTAINER_TAG"
+        operator-sdk build --image-build-args "${args[*]}" "$CONTAINER_IMAGE:$CONTAINER_TAG" || exit 3
     else
         docker build "${args[@]}" -t "$CONTAINER_IMAGE:$CONTAINER_TAG" . || exit 3
     fi
